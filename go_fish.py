75a46ca58df564fb6923a5d89ac6853e5062eeb4
yanxia97 <isaacyan@umich.edu>
2018-09-22 15:33:37 -0400

    deal the cards

---
+++ go_fish.py
@@ -0,0 +1,16 @@
+import cards
+
+# SI 507 Fall 2018
+# Homework 3 Extra Credit 1 - Code
+
+# COMMENT YOUR CODE WITH:
+# Section Day/Time: 004,9/19/2018,17:30-19:00
+# People you worked with: wowwh
+
+if __name__ == "__main__":
+	deck = cards.Deck()
+	deck.shuffle()
+	hand1, hand2 = deck.deal(2,7)
+	# deal the cards to the two players
+	# made by isaacyan
+
