53f98c397050d31de6deaa3cae1338f9e84181cd
Hao Wu <wowwh@umich.edu>
2018-09-22 23:36:35 -0400

    fill in the two functions

--- go_fish.py
+++ go_fish.py
@@ -12,43 +12,96 @@ import cards
 # return: nothing
 # edited by wowwh, commented by isaacyan
 def ask(hand1,hand2,deck):
-	pass
+    rank=int(input("Please choose a card rank you would like to ask the other player if they have (between 1-13): "))	
+    replace_card=[]    
+    fish = False
+    for card in hand2.cards:
+        if card.rank_num == rank:
+            fish = True
+            replace_card.append(card)
+    for card in replace_card:
+        hand1.add_card(hand2.remove_card(card))
+
+    print("Player asked for " + str(rank))
+
+
+    if fish ==True and len(deck.cards)>0 :
+        print ("The other player had "+ str(rank))
+        print("Please continue to choose card!")
+        ask(hand1,hand2,deck)
+    elif fish == False and len(deck.cards)>0 :
+        hand1.draw(deck)
+        print ("The player had to go fish!")
+        if hand1.cards[-1].rank_num == rank:
+            print("Please continue to choose card!")
+            ask(hand1,hand2,deck)
+        else:
+            print("Next turn!")
+    else:
+        print("Next turn!")
+        pass
+
+
+
+
 
 # check whether someone has 4 cards with the same rank
 # param: hand of a player
 # return: a list of the books (a list with a rank number of a null list)
 # edited by wowwh, commented by isaacyan
 def check(hand):
-	pass
+    book=[]
+    card_list={}
+    for card in hand.cards:
+        if card.rank_num not in card_list.keys():
+            card_list[card.rank_num]=1
+        else:
+            card_list[card.rank_num]+=1
+    for each_rank in card_list.keys():
+        if card_list[each_rank] == 4:
+            print ("The player get a book of "+ str(each_rank))
+            for card in hand.cards:
+                if card.rank_num == each_rank:
+                    hand.remove_card(card)
+            book.append(each_rank)
+    return book
+
+
+
+
 
 # iterate for each turn until all the books are shown and jufge who wins the game.
 # param: hands of the players and the remaing cards in the deck
 # return:  nothing
 # edited by isaacyan
 def play(hand1, hand2, deck):
-	turn = 0 # show whose turn is
-	book1 = []
-	book2 = []
-	while True:
-		if turn/2==0:
-			ask(hand1, hand2)
-			book1 += check(hand1)
-		else:
-			ask(hand2, hand1)
-			book2 += check(hand2)
-		turn += 1
-		if len(book1)+len(book2)==13:
-			if len(book1)>len(book2):
-				print("Player1 wins!")
-			else:
-				print("Player2 wins!")
-			break
+    turn = 0 # show whose turn is
+    book1 = []
+    book2 = []
+    while True:
+        if turn/2==0:
+            print("Player1's turn!")
+            ask(hand1, hand2, deck)
+            book1 += check(hand1)
+        else:
+            print("Player2's turn!")
+            ask(hand2, hand1, deck)
+            book2 += check(hand2)
+        turn += 1
+        if len(book1)+len(book2)==13:
+            if len(book1)>len(book2):
+                print("Player1 wins!")
+            else:
+                print("Player2 wins!")
+            break
 
 
 if __name__ == "__main__":
-	deck = cards.Deck()
-	deck.shuffle()
-	hand1, hand2 = deck.deal(2,7) # deal the cards to the two players
-	play(hand1, hand2, deck) # the player with hand 1 goes first
+    deck = cards.Deck()
+    deck.shuffle()
+    hands= deck.deal(2,7) # deal the cards to the two players
+    hand1=hands[0]
+    hand2=hands[1]
+    play(hand1, hand2, deck) # the player with hand 1 goes first
 	# edited by isaacyan
 
